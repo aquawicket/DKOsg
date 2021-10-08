015e2ac6cb641ced9cd94397719e5b61421ff319
aquawicket <aquawicket@hotmail.com>
2021-10-08 14:50:57 -0700

    commit from git

--- DKApps/DKOSGRml/assets/test.js
+++ DKApps/DKOSGRml/assets/test.js
@@ -9,10 +9,10 @@ function test_Init()
 ////////////////////////////
 function test_OnEvent(event)
 {
-	DKLog("test_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKDEBUG);
+	console.log("test_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKDEBUG);
 	
 	if(DK_Type(event,"keydown")){
-		DKLog("keydown "+DK_GetValue(event)+" \n");
+		console.log("keydown "+DK_GetValue(event)+" \n");
 		if(DK_GetValue(event) == "4"){ //ANDROID_BACK
 			DK_Exit();
 		}
